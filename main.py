80813b38beab1b34a9cf50d3cc6f1c4ab50e4a20
psmalz12 <144242023+psmalz12@users.noreply.github.com>
2024-04-17 09:28:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from env import Grid
 from visual import GridVisual
 
+# need to add a loop here for the num of iter 16/04
 if __name__ == "__main__":
     grid = Grid(state=(5, 1))  # the start location
     print('//////// agent current Location "2" ////////')
@@ -8,4 +9,6 @@ if __name__ == "__main__":
     grid.action(1000)  # number of actions/iterations in the environment
 
     
+# will add random action choice  16/04
+# add print action, reward, and term (achieve the goal or not) 
 
