0438a565c31fa74ab2df8da582968045a7b77605
psmalz12 <144242023+psmalz12@users.noreply.github.com>
2024-04-17 09:10:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     grid = Grid(state=(5, 1))  # the start location
     print('//////// agent current Location "2" ////////')
     GridVisual.show_board(grid)
-    grid.action(100)  # number of actions/iterations in the environment
+    grid.action(1000)  # number of actions/iterations in the environment
 
-    # loop
+    
 
